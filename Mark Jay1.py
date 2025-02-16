ed17fd05b67ca158dab272f884324aa1197f9aa7
m4rkj-ay <markjaymadera14@gmail.com>
2025-02-16 22:43:11 +0800

    Add files via upload

---
+++ Mark Jay1.py
@@ -0,0 +1,38 @@
+DATA = {
+    "Inept": "Inept means lacking skill, ability, or competence",
+    "Munificent": "Munificent means being extremely generous or giving",
+    "Prolific": "Prolific means producing a great amount of something, especially in creative or intellectual work",
+    "Yearning": "Yearning means experiencing a deep, emotional longing or strong desire for something often unattanable or distant",
+    "Pensive": "Pensive means deeply thoughtful, often in a wistful or sad way",
+    "Wistful": "Wistful means full of longing or gentle sadness",
+    "Fervent": "Fervent means displaying an intense passion, enthusiasm, or dedication toward something, often within deep emotional commitmeny",
+    "Stoic": "Stoic means enduring pain, hardship, or difficult situations wihout displaying emotion, complaints, or distress",
+    "Indignant": "Indignant means feeling strong anger, resentment, or fustration due to unfair treatment or peeceived injustice",
+    "Despondent": "Despondent means feeling hopeless or in low spirits",
+    "Elated": "Elated means filled with joy or extreme happiness", 
+    "Poignant": "Poignant means evoking deep feelings of sadness or symphaty",
+    "Sanguine": "Sanguine means feeling optimistic or positive, especially in challenging situations",
+    "Maudlin": "Maudlin means overly sentimental, often in a self-pitying way",
+    "Reverent": "Reverent means feeling or showing deep and solemn respect",
+    "Zealous": "Zealous means showing great energy or enthusiasm in pursuit of a cause or objective",
+    "Sagacious": "Sagacious means having or showing keen mental discernment and good judgement",
+    "Lachrymose": "Lachrymose means prone to tears or causing feeling of sadness",
+    "Intrepid": "Intrepid means fearless and adventurous, often in the face of danger",
+    "Equanimity": "Equanimity means remaining calm and composed under stress or pressure",
+    "Deft": "Deft means skillful and quick in movement or action",
+    "Convoluted": "Covoluted means overly complex or difficult to follow",
+    "Ineffable": "Too good or extreme to be expressed or described in words",
+    "Gunnen": "To find happiness in someone else's happiness because that's how much you love them",
+    "Metanoia": "The journey to change one's mind, heart, self or way of life; Spiritual conversation",
+    "Elysian": "Beautiful of creative; divinely inspired; peaceful and perfect",
+    "Euphoria": "A feeling or state of intense excitement and happiness",
+    "Epiphany": "A moment when you suddenly feel that you understand, or suddenly become conscious of something that is very important to you",
+    "Aesthete": "Someone with deep sensitivity to the beauty of art or nature",
+    "Redemancy": "The actn of loving the one who loves you; A love returned in full",
+}
+a = input ("Say. ")
+print (DATA[a])
+
+
+ 
+	 
